41a4375634e5326d416f26b0d3f1c655d9e80c26
tc-imba <yihao002@e.ntu.edu.sg>
2020-10-10 02:23:58 +0800

    adjust the position of date

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -14,20 +14,20 @@ const PostCard = (props) => {
   return (
     <div className={style.postCard}>
       <Link to={Utils.resolvePageUrl(frontmatter.path)}>
-        <div
-          className={style.postCardImg}
-          style={{
-            backgroundImage: `url(${frontmatter && frontmatter.cover ? frontmatter.cover.childImageSharp.fluid.src : ''})`,
-          }}
-        />
+        <div>
+          <span className={style.dateHolder}>{frontmatter ? moment(frontmatter.date).format('MMM Do YYYY') : ''}</span>
+          <div
+            className={style.postCardImg}
+            style={{
+              backgroundImage: `url(${frontmatter && frontmatter.cover ? frontmatter.cover.childImageSharp.fluid.src : ''})`,
+            }}
+          />
+        </div>
         <div className={style.mrTp20}>
           <h3>{frontmatter ? frontmatter.title : ''}</h3>
         </div>
       </Link>
       <Row align="middle" gutter={[0, 8]}>
-        <Col xs>
-          <span className={style.dateHolder}>{frontmatter ? moment(frontmatter.date).format('MMM Do YYYY') : ''}</span>
-        </Col>
         { frontmatter.tags.map((tag) => (<PostTag tag={tag} />))}
       </Row>
       <Link to={Utils.resolvePageUrl(frontmatter.path)}>
