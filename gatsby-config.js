dae4be090a9226ffd25316246819a3d7554517ef
tc-imba <liuyh615@sjtu.edu.cn>
2020-11-18 03:29:05 +0800

    fix pathPrefix in dev mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,13 @@
 const config = require('./config');
 const plugins = require('./gatsby-config.plugins');
 
+let { pathPrefix } = config;
+if (process && process.env.NODE_ENV !== 'production') {
+  pathPrefix = '';
+}
+
 module.exports = {
-  pathPrefix: config.pathPrefix,
+  pathPrefix,
   siteMetadata: {
     // Data used by some gatsby plugins
     siteUrl: config.siteUrl,
