0f19796ddfdaca5a4d69ab446f68ce4dd17e5b96
BoYanZh <bomingzh@sjtu.edu.cn>
2021-12-31 01:53:03 +0800

    feat: update icon

--- gatsby-theme-academic/gatsby-config.js
+++ gatsby-theme-academic/gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = ({
         theme_color: '#304CFD',
         display: 'standalone',
         // icon: `${__dirname}/src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `favicon.ico`, // This path is relative to the root of the site.
         legacy: true, // this will add apple-touch-icon links to <head>. Required for
         // versions prior to iOS 11.3.
       },
